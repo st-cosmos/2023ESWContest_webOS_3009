9b00e6c10d28be386c403d30d5eb6089e77a0f53
SunYoung710 <tdrubytd@gmail.com>
2023-09-30 16:48:14 +0900

    fixed light-therapy css

--- server/dev/webserver/test_client.py
+++ server/dev/webserver/test_client.py
@@ -2,7 +2,7 @@ import requests
 import re
 
 # 서버의 URL과 데이터 설정
-server_url = "http://172.30.1.7:9999/Diary"  # 서버의 IP 주소와 포트로 변경
+server_url = "http://localhost:9999/Diary"  # 서버의 IP 주소와 포트로 변경
 data = {
     "text": "개미는 뚠뚠. 오늘도 뚠뚠. 열심히 일을 하네."  # 서버로 전송할 데이터
 }
